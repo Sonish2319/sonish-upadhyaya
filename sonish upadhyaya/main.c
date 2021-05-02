7516813e53b780b1388b55534ca5548fd5804bed
Sonish <sonish2319@gmail.com>
2021-05-02 21:14:32 +0545

    Initial Commit

---
+++ sonish upadhyaya/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sonish upadhyaya
+//
+//  Created by sonish upadhyay on 5/2/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
